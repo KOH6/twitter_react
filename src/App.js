61167f3acfeb9e5ad683477f485d2a12d2f6da03
KOH6 <minagk38+engineer@gmail.com>
2023-11-07 23:51:53 +0900

    メール承認後の遷移先をホームに変更

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import { SignUp } from "./containers/SignUp.jsx";
+import { PostIndex } from "./containers/PostIndex.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/signup" element={<SignUp />}></Route>
+        <Route exact path="/home" element={<PostIndex />}></Route>
       </Routes>
     </Router>
   );
--- src/urls/index.js
+++ src/urls/index.js
@@ -2,4 +2,4 @@ const DEFAULT_API_LOCALHOST = "http://localhost:3000/api/v1";
 
 export const users = `${DEFAULT_API_LOCALHOST}/users`;
 
-export const CONFIRM_SUCCESS_URL = "http://localhost:3001/signup";
+export const CONFIRM_SUCCESS_URL = "http://localhost:3001/home";
