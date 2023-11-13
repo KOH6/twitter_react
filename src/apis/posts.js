7a93299f17f1e49f266e25a924bf488aa555937b
KOH6 <minagk38+engineer@gmail.com>
2023-11-14 02:10:16 +0900

    fetchPostsのデフォルト引数削除

--- src/apis/posts.js
+++ src/apis/posts.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 import { tweets, images } from "../urls/index";
 
-export const LIMIT = 10;
-
 export const createPost = (post, headers) => {
   return axios.post(tweets, { post: post }, { headers: headers });
 };
@@ -12,6 +10,6 @@ export const attachImages = (formData, headers) => {
   return axios.post(images, formData, { headers: headers });
 };
 
-export const fetchPosts = (limit = LIMIT, offset) => {
+export const fetchPosts = (limit, offset) => {
   return axios.get(`${tweets}?limit=${limit}}&offset=${offset}}`);
 };
