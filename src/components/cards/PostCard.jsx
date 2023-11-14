c26708bb55a772ffdb5a5ef6eaf0379fc8fe8cdc
KOH6 <minagk38+engineer@gmail.com>
2023-11-14 15:08:58 +0900

    カードクリック時に詳細画面に遷移

--- src/components/cards/PostCard.jsx
+++ src/components/cards/PostCard.jsx
@@ -2,64 +2,72 @@ import React from "react";
 
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { Box, Grid, Stack, Typography } from "@mui/material";
+import { Box, CardActionArea, Grid, Stack, Typography } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import { useNavigate } from "react-router-dom";
 
 export const PostCard = (props) => {
   const { post } = props;
+  const navigate = useNavigate();
 
   return (
     <Card
       variant="outlined"
       sx={{ width: "50%", margin: "0 auto", textAlign: "center" }}
     >
-      <CardContent>
-        <Grid container>
-          <Grid item xs={1} sx={{ textAlign: "left" }}>
-            <PersonIcon fontSize="large" />
-          </Grid>
-          <Grid item xs={11}>
-            <Typography
-              variant="body1"
-              sx={{ px: 3, textAlign: "left" }}
-              gutterBottom
-            >
-              {post.content}
-            </Typography>
-            {post.image_paths.map((image_path, index) => (
-              <img
-                key={index}
-                src={image_path}
-                style={{
-                  width: "80%",
-                  margin: "1rem auto",
-                  borderRadius: "10px",
-                }}
-                alt=""
-              />
-            ))}
-            <Box sx={{ mt: 2 }}>
-              <Stack
-                spacing={{ xs: 1, sm: 2 }}
-                direction="row"
-                useFlexGap
-                flexWrap="wrap"
-                justifyContent="space-around"
+      <CardActionArea
+        onClick={() => {
+          navigate(`/${post.id}`);
+        }}
+      >
+        <CardContent>
+          <Grid container>
+            <Grid item xs={1} sx={{ textAlign: "left" }}>
+              <PersonIcon fontSize="large" />
+            </Grid>
+            <Grid item xs={11}>
+              <Typography
+                variant="body1"
+                sx={{ px: 3, textAlign: "left" }}
+                gutterBottom
               >
-                <ChatBubbleOutlineIcon />
-                <RepeatIcon />
-                <FavoriteBorderIcon />
-                <BookmarkBorderIcon />
-              </Stack>
-            </Box>
+                {post.content}
+              </Typography>
+              {post.image_paths.map((image_path, index) => (
+                <img
+                  key={index}
+                  src={image_path}
+                  style={{
+                    width: "80%",
+                    margin: "1rem auto",
+                    borderRadius: "10px",
+                  }}
+                  alt=""
+                />
+              ))}
+              <Box sx={{ mt: 2 }}>
+                <Stack
+                  spacing={{ xs: 1, sm: 2 }}
+                  direction="row"
+                  useFlexGap
+                  flexWrap="wrap"
+                  justifyContent="space-around"
+                >
+                  <ChatBubbleOutlineIcon />
+                  <RepeatIcon />
+                  <FavoriteBorderIcon />
+                  <BookmarkBorderIcon />
+                </Stack>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
-      </CardContent>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 };
