c915e20bb6897d0fe1ebc6d5acc058555ab5a854
KOH6 <minagk38+engineer@gmail.com>
2024-01-07 15:06:50 +0900

    プロフィール更新ボタンの表示

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { RecoilRoot } from "recoil";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { ThemeProvider, createTheme } from "@mui/material";
-import { blue } from "@mui/material/colors";
+import { blue, grey } from "@mui/material/colors";
 
 import { SignUp } from "./containers/SignUp.jsx";
 import { LogIn } from "./containers/LogIn";
@@ -19,6 +19,7 @@ import { Spinner } from "./components/utils/Spinner";
 const theme = createTheme({
   palette: {
     primary: blue,
+    secondary: grey,
   },
 });
 
