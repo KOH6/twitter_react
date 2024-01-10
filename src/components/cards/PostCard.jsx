f4961fa54eb68025aa4bc5aebc54c596bae96cfc
KOH6 <minagk38+engineer@gmail.com>
2024-01-11 00:37:15 +0900

    固定の内容でコメントレコード作成処理

--- src/components/cards/PostCard.jsx
+++ src/components/cards/PostCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Cookies from "js-cookie";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { useNavigate } from "react-router-dom";
 
@@ -32,6 +33,7 @@ import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import { createComment } from "../../apis/comments.js";
 
 const HeaderTitle = (props) => {
   const { header, subHeader } = props;
@@ -83,6 +85,25 @@ export const PostCard = (props) => {
     },
   ];
 
+  const footerItems = [
+    {
+      icon: <ChatBubbleOutlineIcon />,
+      onClick: (e) => handleCreateComment(e),
+    },
+    {
+      icon: <RepeatIcon />,
+      onClick: (e) => handleCreateComment(e),
+    },
+    {
+      icon: <FavoriteBorderIcon />,
+      onClick: (e) => handleCreateComment(e),
+    },
+    {
+      icon: <BookmarkBorderIcon />,
+      onClick: (e) => handleCreateComment(e),
+    },
+  ];
+
   /**
    * 確認ダイアログ上の情報
    */
@@ -123,6 +144,33 @@ export const PostCard = (props) => {
     } finally {
       setLoading(false);
       setConfirming((prev) => ({ ...prev, isOpen: false }));
+    }
+  };
+
+  const handleCreateComment = async (e) => {
+    e.stopPropagation();
+
+    try {
+      setLoading(true);
+
+      const headers = {
+        "access-token": Cookies.get("_access_token"),
+        client: Cookies.get("_client"),
+        uid: Cookies.get("_uid"),
+      };
+
+      // TODO:入力内容を画面から受け取る
+      const comment = {
+        post_id: post.id,
+        content: "form react",
+      };
+
+      // 本文を登録する
+      const res = await createComment(comment, headers);
+    } catch (err) {
+      console.log("err", err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -222,10 +270,27 @@ export const PostCard = (props) => {
                   flexWrap="wrap"
                   justifyContent="space-around"
                 >
-                  <ChatBubbleOutlineIcon />
-                  <RepeatIcon />
-                  <FavoriteBorderIcon />
-                  <BookmarkBorderIcon />
+                  {footerItems.map((item, index) => (
+                    <Box
+                      key={`post-${post.id}-fotterItem-${index}`}
+                      sx={{
+                        zIndex: 10000,
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "2rem",
+                        width: "2rem",
+                        "&:hover": {
+                          background: "#E4E4E4",
+                          borderRadius: "50%",
+                          opacity: 0.99,
+                        },
+                      }}
+                      onClick={item.onClick}
+                    >
+                      {item.icon}
+                    </Box>
+                  ))}
                 </Stack>
               </Box>
             </Grid>
