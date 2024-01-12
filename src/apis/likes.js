e676237c6960aae2cca6db8582e64029a365639d
KOH6 <minagk38+engineer@gmail.com>
2024-01-12 10:32:07 +0900

    headers取得処理を都度呼び出す形に修正

--- src/apis/likes.js
+++ src/apis/likes.js
@@ -1,18 +1,16 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 
 import { tweets } from "../urls/index";
-
-const headers = {
-  "access-token": Cookies.get("_access_token"),
-  client: Cookies.get("_client"),
-  uid: Cookies.get("_uid"),
-};
+import { createDefaultHeaders } from "../lib/utility.js";
 
 export const createLike = (id) => {
-  return axios.post(`${tweets}/${id}/favorites`, null, { headers: headers });
+  return axios.post(`${tweets}/${id}/favorites`, null, {
+    headers: createDefaultHeaders(),
+  });
 };
 
 export const deleteLike = (id) => {
-  return axios.delete(`${tweets}/${id}/favorites`, { headers: headers });
+  return axios.delete(`${tweets}/${id}/favorites`, {
+    headers: createDefaultHeaders(),
+  });
 };
--- src/apis/reposts.js
+++ src/apis/reposts.js
@@ -1,18 +1,16 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 
 import { tweets } from "../urls/index";
-
-const headers = {
-  "access-token": Cookies.get("_access_token"),
-  client: Cookies.get("_client"),
-  uid: Cookies.get("_uid"),
-};
+import { createDefaultHeaders } from "../lib/utility.js";
 
 export const createRepost = (id) => {
-  return axios.post(`${tweets}/${id}/retweets`, null, { headers: headers });
+  return axios.post(`${tweets}/${id}/retweets`, null, {
+    headers: createDefaultHeaders(),
+  });
 };
 
 export const deleteRepost = (id) => {
-  return axios.delete(`${tweets}/${id}/retweets`, { headers: headers });
+  return axios.delete(`${tweets}/${id}/retweets`, {
+    headers: createDefaultHeaders(),
+  });
 };
