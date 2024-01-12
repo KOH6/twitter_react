585856fc99adbaa6a57ef0eccaebb7c970107764
KOH6 <minagk38+engineer@gmail.com>
2024-01-12 14:05:27 +0900

    フォロー、フォロー解除処理

--- src/components/details/UserDetail.jsx
+++ src/components/details/UserDetail.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -9,9 +10,116 @@ import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
 import { UserEditModal } from "../modals/UserEditModal";
 
+import { currentUserState, loadingState } from "../../globalStates/atoms";
+import { fetchUser } from "../../apis/users";
+import { createFollow, deleteFollow } from "../../apis/follows.js";
+
+const LoggedInButton = (props) => {
+  return (
+    <Button
+      variant="outlined"
+      color="black"
+      size="large"
+      onClick={props.onClick}
+      sx={{
+        borderRadius: 50,
+        fontWeight: "bold",
+      }}
+    >
+      プロフィールを編集
+    </Button>
+  );
+};
+
+const FollowingButton = (props) => {
+  return (
+    <Button
+      variant="contained"
+      color="black"
+      size="large"
+      onClick={props.onClick}
+      sx={{
+        borderRadius: 50,
+        fontWeight: "bold",
+      }}
+    >
+      フォロー
+    </Button>
+  );
+};
+
+const UnFollowingButton = (props) => {
+  return (
+    <Button
+      variant="outlined"
+      color="black"
+      size="large"
+      onClick={props.onClick}
+      sx={{
+        width: "10rem",
+        borderRadius: 50,
+        fontWeight: "bold",
+        // ホバー時にボタンデザインを変更する
+        "& .hoverText": {
+          display: "none",
+        },
+        "&:hover": {
+          background: "#FFEDEC",
+          borderColor: "#FEC9CE",
+          transition: "0s",
+          "& .defaultText": { display: "none" },
+          "& .hoverText": { display: "inline" },
+        },
+      }}
+    >
+      <p className="defaultText">フォロー中</p>
+      <p className="hoverText" style={{ color: "#F4202E" }}>
+        フォロー解除
+      </p>
+    </Button>
+  );
+};
+
 export const UserDetail = (props) => {
   const { user, isLoggedInUser } = props;
-  const [open, setOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
+  const setLoading = useSetRecoilState(loadingState);
+  const [openEditModal, setOpenEditModal] = useState(false);
+
+  // ログインユーザのfolloweesに表示ユーザが含まれていたらフォローしている。明示的にBoolean型にキャストする。
+  const isFollowing = !!currentUser.followees.find(
+    (followee) => followee.id === user.id
+  );
+
+  const handleClickFollowing = async () => {
+    try {
+      setLoading(true);
+
+      await createFollow(user.user_name);
+
+      const res = await fetchUser(currentUser.user_name);
+      setCurrentUser(res.data);
+    } catch (err) {
+      console.log("err", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleClickUnFollowing = async () => {
+    try {
+      setLoading(true);
+
+      await deleteFollow(user.user_name);
+
+      const res = await fetchUser(currentUser.user_name);
+      setCurrentUser(res.data);
+    } catch (err) {
+      console.log("err", err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -37,22 +145,12 @@ export const UserDetail = (props) => {
             />
           }
           action={
-            isLoggedInUser && (
-              <Button
-                type="submit"
-                variant="outlined"
-                color="black"
-                size="large"
-                onClick={() => {
-                  setOpen(true);
-                }}
-                sx={{
-                  borderRadius: 50,
-                  fontWeight: "bold",
-                }}
-              >
-                プロフィールを編集
-              </Button>
+            isLoggedInUser ? (
+              <LoggedInButton onClick={() => setOpenEditModal(true)} />
+            ) : isFollowing ? (
+              <UnFollowingButton onClick={() => handleClickUnFollowing()} />
+            ) : (
+              <FollowingButton onClick={() => handleClickFollowing()} />
             )
           }
         />
@@ -70,7 +168,10 @@ export const UserDetail = (props) => {
         </CardContent>
       </Card>
       {/* 編集モーダル */}
-      <UserEditModal open={open} setOpen={setOpen}></UserEditModal>
+      <UserEditModal
+        open={openEditModal}
+        setOpen={setOpenEditModal}
+      ></UserEditModal>
     </>
   );
 };
