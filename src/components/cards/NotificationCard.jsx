df974441a91dcea9d2890f01bc549d845c252093
KOH6 <minagk38+engineer@gmail.com>
2024-01-13 16:34:42 +0900

    投稿いいねの通知カードデザイン作成

--- src/components/cards/NotificationCard.jsx
+++ src/components/cards/NotificationCard.jsx
@@ -3,14 +3,17 @@ import React from "react";
 import {
   Avatar,
   Card,
+  CardActions,
   CardContent,
   CardHeader,
   Grid,
   Typography,
 } from "@mui/material";
-import { PostCardHeaderTitle } from "../PostCardHeaderTitle";
-import { formatDateTime } from "../../lib/utility";
+
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
 import { CommentCard } from "./CommentCard";
+import { useNavigate } from "react-router-dom";
 
 const FollowCard = (props) => {
   const { notification } = props;
@@ -19,35 +22,82 @@ const FollowCard = (props) => {
 
 const PostShowingCard = (props) => {
   const { notification } = props;
+  const navigate = useNavigate();
 
   return (
-    <Card variant="outlined">
+    <Card
+      variant="outlined"
+      sx={{
+        textAlign: "center",
+        borderRight: "none",
+        borderLeft: "none",
+        borderRadius: "0%",
+      }}
+    >
       <CardContent>
         <Grid container>
           <Grid item xs={1} sx={{ textAlign: "left" }}>
-            <Avatar
+            <FavoriteIcon
               sx={{
+                color: "#F91780",
                 height: "4vh",
                 width: "4vh",
+                ml: 1,
+                mt: 1,
               }}
-              alt={`${notification.sender.name}`}
-              src={`${notification.sender.profile_image_path}`}
             />
           </Grid>
           <Grid item xs={11}>
             <CardHeader
               sx={{ p: 1 }}
-              title={
-                <PostCardHeaderTitle
-                  header={notification.sender.name}
-                  subHeader={`@${
-                    notification.sender.user_name
-                  }・${formatDateTime(
-                    new Date(notification.target.created_at)
-                  )}`}
-                />
+              avatar={
+                <CardActions
+                  sx={{
+                    zIndex: (theme) => theme.zIndex.appBar + 1,
+                    p: 0,
+                    m: 0,
+                  }}
+                  disableSpacing
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    navigate(`/${notification.sender.user_name}`);
+                  }}
+                >
+                  <Avatar
+                    sx={{
+                      height: "4vh",
+                      width: "4vh",
+                      "&:hover": {
+                        cursor: "pointer",
+                        opacity: "0.8",
+                      },
+                    }}
+                    alt={`${notification.sender.name}`}
+                    src={`${notification.sender.profile_image_path}`}
+                  />
+                </CardActions>
               }
             />
+            <Typography
+              variant="body1"
+              sx={{
+                px: 1,
+                textAlign: "left",
+                whiteSpace: "pre-line",
+              }}
+              gutterBottom
+            >
+              <strong
+                style={{
+                  fontWeight: "bold",
+                  paddingRight: "0.5rem",
+                  color: "black",
+                }}
+              >
+                {notification.sender.name}
+              </strong>
+              さんがあなたのポストにいいねしました
+            </Typography>
             <Typography
               variant="body1"
               sx={{ px: 1, textAlign: "left", whiteSpace: "pre-line" }}
