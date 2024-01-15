19d9532633c19a80f1cd8f16e9c73fb57727aa22
KOH6 <minagk38+engineer@gmail.com>
2024-01-15 22:57:56 +0900

    投稿明細のフッター生成をhooks化

--- src/components/cards/PostCard.jsx
+++ src/components/cards/PostCard.jsx
@@ -20,26 +20,19 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import RepeatIcon from "@mui/icons-material/Repeat";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 import { CommentCreateModal } from "../modals/CommentCreateModal.jsx";
 import { PostCardHeaderTitle } from "../PostCardHeaderTitle.jsx";
-import { createRepost, deleteRepost } from "../../apis/reposts.js";
-import { createLike, deleteLike } from "../../apis/likes.js";
-import { fetchUser } from "../../apis/users.js";
+
 import { useGeneratePostCardMenuItems } from "../../hooks/posts/useGeneratePostCardMenuItems.jsx";
+import { useGeneratePostCardFooterItems } from "../../hooks/posts/useGeneratePostCardFooterItems.jsx";
 
 export const PostCard = (props) => {
-  // 削除後の後処理はページごとに異なるので、propsで渡す
   const { post, afterDeletePost, afterCreateComment, reFetch } = props;
   const [open, setOpen] = useState(false);
 
-  const [currentUser, setCurrentUser] = useRecoilState(currentUserState);
-  const setLoading = useSetRecoilState(loadingState);
+  const navigate = useNavigate();
 
   const menuItems = useGeneratePostCardMenuItems({
     record: post,
@@ -48,90 +41,11 @@ export const PostCard = (props) => {
     reFetch: reFetch,
   });
 
-  const navigate = useNavigate();
-
-  const alreadyReposted =
-    currentUser.retweets.filter((item) => item.id === post.id).length !== 0;
-  const alreadyLiked =
-    currentUser.likes.filter((item) => item.id === post.id).length !== 0;
-
-  const footerItems = [
-    // コメント
-    {
-      color: "#1E9BF0",
-      background: "#E7EFF8",
-      icon: (
-        <>
-          <ChatBubbleOutlineIcon />
-          {post.comment_count !== 0 && (
-            <Typography>{post.comment_count}</Typography>
-          )}
-        </>
-      ),
-      onClick: (e) => {
-        e.stopPropagation();
-        setOpen(true);
-      },
-    },
-    // リツイート
-    {
-      color: "#00BA7C",
-      background: "#E7F2EC",
-      alreadyDone: alreadyReposted,
-      icon: (
-        <>
-          <RepeatIcon />
-          {post.retweet_count !== 0 && (
-            <Typography>{post.retweet_count}</Typography>
-          )}
-        </>
-      ),
-      onClick: async (e) => {
-        e.stopPropagation();
-        await handleClickIcon(alreadyReposted, createRepost, deleteRepost);
-      },
-    },
-    // いいね
-    {
-      color: "#F91780",
-      background: "#FAE6ED",
-      alreadyDone: alreadyLiked,
-      icon: (
-        <>
-          <FavoriteBorderIcon />
-          {post.like_count !== 0 && <Typography>{post.like_count}</Typography>}
-        </>
-      ),
-      onClick: async (e) => {
-        e.stopPropagation();
-        await handleClickIcon(alreadyLiked, createLike, deleteLike);
-      },
-    },
-    {
-      icon: <BookmarkBorderIcon />,
-      onClick: () => {},
-    },
-  ];
-
-  const handleClickIcon = async (alreadyDone, createRecord, deleteRecord) => {
-    try {
-      setLoading(true);
-
-      if (alreadyDone) {
-        await deleteRecord(post.id);
-      } else {
-        await createRecord(post.id);
-      }
-
-      await reFetch();
-      const res = await fetchUser(currentUser.user_name);
-      setCurrentUser(res.data);
-    } catch (err) {
-      console.log("err", err);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const footerItems = useGeneratePostCardFooterItems({
+    post: post,
+    setOpen: setOpen,
+    reFetch: reFetch,
+  });
 
   const handleClickUser = (e) => {
     e.stopPropagation();
